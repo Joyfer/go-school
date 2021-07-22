8e7cbfce730767cd50e8ee38768e2ce81de24e5c
Joyfer <joyferramos@gmail.com>
2021-07-22 00:26:52 -0600

    Homeworks page created

--- components/resources/card/CardTitle.tsx
+++ components/resources/card/CardTitle.tsx
@@ -1,15 +1,23 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement } from "react";
 
 interface Props {
- children?: any;
- icon?: ReactElement;
+  children?: any;
+  icon?: ReactElement;
+  subtitle?: string;
 }
 
-export default function CardTitle({children, icon}: Props): ReactElement {
-    return (
-        <div className="mb-3 flex justify-between">
-          <p className="text-black font-bold text-lg">{children}</p>
-          {icon ? icon : ""}
-        </div>
-    )
+export default function CardTitle({
+  children,
+  icon,
+  subtitle,
+}: Props): ReactElement {
+  return (
+    <div className="mb-3">
+      <div className="flex justify-between">
+        <p className="text-black font-bold text-xl">{children}</p>
+        {icon ? icon : ""}
+      </div>
+      {subtitle ? <span className="text-accent font-bold">{subtitle}</span> : ""}
+    </div>
+  );
 }
---
+++ pages/homework/index.tsx
@@ -0,0 +1,55 @@
+import React, { ReactElement } from "react";
+import Link from "next/link";
+import Layout from "../../components/layout/Layout";
+import CardLayout from "../../components/resources/card/CardLayout";
+import CardTitle from "../../components/resources/card/CardTitle";
+import CardText from "../../components/resources/card/CardText";
+import CardActions from "../../components/resources/card/CardActions";
+import Badge from "../../components/resources/badge/Badge";
+
+import Icon from "@mdi/react";
+import { mdiSubdirectoryArrowRight } from "@mdi/js";
+
+const LinkCard = React.forwardRef(
+  ({ onClick, href, children }: any, ref: any) => {
+    return (
+      <a href={href} onClick={onClick} ref={ref}>
+        {children}
+      </a>
+    );
+  }
+);
+
+export default function homeworkIndex(): ReactElement {
+  return (
+    <Layout>
+      <div className="text-center mt-10">
+        <h1 className="mb-4">Tareas</h1>
+        <p>
+         Tienes 3 tares nuevas de: Biologia, Matemática.
+        </p>
+      </div>
+      <div className="mt-6 space-y-2">
+        {["Tareas", "Blogs"].map((el) => (
+          <CardLayout key={el}>
+            <Link href="/about" passHref>
+              <LinkCard>
+                <CardTitle subtitle="Biologia">{el}</CardTitle>
+                <CardText>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Quasi eos adipisci nobis velit.
+                </CardText>
+                <CardActions>
+                    <div></div>
+                  <Icon path={mdiSubdirectoryArrowRight} size={1.2}>
+                    hola
+                  </Icon>
+                </CardActions>
+              </LinkCard>
+            </Link>
+          </CardLayout>
+        ))}
+      </div>
+    </Layout>
+  );
+}
